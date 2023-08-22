35960b3ec5997659381354b16ffb737721e3210b
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-22 23:52:25 +0500

    Add array of To Do tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+// Array of tasks
+const tasks = [
+  {
+    description: "Task 1",
+    completed: false,
+    index: 1,
+  },
+  {
+    description: "Task 2",
+    completed: true,
+    index: 2,
+  },
+  {
+    description: "Task 3",
+    completed: false,
+    index: 3,
+  },
+];
