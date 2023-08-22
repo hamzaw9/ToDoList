9a137a1c49d5c565b89d9a64347dc26ef2cbdb8c
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-23 00:52:40 +0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import "./style.css";
+import './style.css';
 
 // Array of tasks
 const tasks = [
   {
-    description: "Task 1",
+    description: 'Task 1',
     completed: false,
     index: 1,
   },
   {
-    description: "Task 2",
+    description: 'Task 2',
     completed: true,
     index: 2,
   },
   {
-    description: "Task 3",
+    description: 'Task 3',
     completed: false,
     index: 3,
   },
 ];
 
 function renderTasks() {
-  const listContainer = document.getElementById("list-container");
+  const listContainer = document.getElementById('list-container');
 
   tasks.forEach((task) => {
     listContainer.innerHTML += ` <div class="list-container-inner">
