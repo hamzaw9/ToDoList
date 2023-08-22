709dd33b80bc2bf3f56d578291f2f0be4393c154
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-23 00:05:56 +0500

    Implement functionality to display tasks dynamically

--- src/index.js
+++ src/index.js
@@ -18,3 +18,16 @@ const tasks = [
     index: 3,
   },
 ];
+
+function renderTasks() {
+  const listContainer = document.getElementById("list-container");
+
+  tasks.forEach((task) => {
+    listContainer.innerHTML += ` <div class="list-container-inner">
+      <input type="checkbox">
+      <span>${task.description}</span>
+      <i class="fa-solid fa-ellipsis-vertical"></i>
+    </div>`;
+  });
+}
+renderTasks();
