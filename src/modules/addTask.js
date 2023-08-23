ecd9202a9ef1600dcf445121b4a6a3c01b803332
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-23 18:05:27 +0500

    Implement add task functionality

---
+++ src/modules/addTask.js
@@ -0,0 +1,11 @@
+const tasks = JSON.parse(localStorage.getItem("Tasks")) || [];
+const addTaskValue = document.querySelector("#add-task").value;
+
+const addTaskToList = () => {
+  const newTask = {
+    description: addTaskValue,
+    completed: false,
+    index: tasks.length + 1,
+  };
+  tasks.push(newTask);
+};
