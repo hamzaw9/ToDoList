65d9f9eb04dbf80fb81e2f02b2b6c50595ad4849
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-23 17:59:38 +0500

    Implement add task button

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-import './style.css';
-
-// Array of tasks
-const tasks = [
-  {
-    description: 'Task 1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Task 2',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 3,
-  },
-];
+import "./style.css";
 
 function renderTasks() {
-  const listContainer = document.getElementById('list-container');
+  const listContainer = document.getElementById("list-container");
 
   tasks.forEach((task) => {
     listContainer.innerHTML += ` <div class="list-container-inner">
